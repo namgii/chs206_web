0db17862fb2f9883cdf6c9287dd16444779600e9
namgi kim <namgi@namgiui-MacBookAir.local>
2024-07-09 12:29:30 +0900

    message

---
+++ script.js
@@ -0,0 +1,17 @@
+// JavaScript 코드는 버튼 클릭 시 영상 링크로 이동하는 역할을 합니다.
+
+document.getElementById('number-button').addEventListener('click', function() {
+    window.location.href = 'number/phone_number.html';
+});
+
+document.getElementById('notion-button').addEventListener('click', function() {
+    window.location.href = 'notion/notion.html';
+});
+
+document.getElementById('time-button').addEventListener('click', function() {
+    window.location.href = 'http://www.xn--s39aj90b0nb2xw6xh.kr/';
+});
+
+document.getElementById('sugges-button').addEventListener('click', function() {
+    window.location.href = 'sugges/suggestion.html';
+});
